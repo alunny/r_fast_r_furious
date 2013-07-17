3a3e74d293f398e2ecfee2a22fa86b01328d3dbe
Andrew Lunny <alunny@twitter.com>
2013-07-17 13:26:57 -0700

    1.2.0: added UMD support

--- fast.js
+++ fast.js
@@ -1,5 +1,16 @@
-var r_fast = /^((The(?=.*the)|2(?=.* 2)) )?Fast (Five|(((and the)|2(?=.*s$)|&(?!.*:)) Furious(( (6|7))|: Tokyo Drift)?))$/;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.r_fast_r_furious = factory();
+  }
+}(this, function () {
+    var r_fast = /^((The(?=.*the)|2(?=.* 2)) )?Fast (Five|(((and the)|2(?=.*s$)|&(?!.*:)) Furious(( (6|7))|: Tokyo Drift)?))$/;
 
-module.exports = function (str) {
-    return r_fast.test(str);
-}
+    return function (str) {
+        return r_fast.test(str);
+    }
+}));
